491d13dd4b51bfa4a965428451e0f15cf272adfd
Garrett Walbridge <gwalbridge@gmail.com>
2012-04-13 20:15:42 -0700

    Added Package Maker project and uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,19 @@
+if [ ! -e "/usr/local/bin/getloc" ]; then
+  echo "getloc doesn't appear to be installed via this installer. Aborting."
+  exit 1
+fi
+
+echo "This will uninstall getloc by removing /usr/local/bin/getloc and /usr/local/share/man/man1/getloc.1"
+printf "Type 'yes' if you are sure you wish to continue: "
+read response
+if [ "$response" == "yes" ]; then
+	sudo rm /usr/local/bin/getloc
+	sudo rm /usr/local/share/man/man1/getloc.1
+	sudo pkgutil --forget com.inspectorg.getloc.getloc.pkg
+	echo "Uninstalled!"
+else
+	echo "Aborted."
+	exit 1
+fi
+
+exit 0
